ba6d7076b590d029c01f0b5b0abad43144ffcab1
szrthk <szrthk@gmail.com>
2025-08-13 15:09:29 +0530

    added entities of facility and slot

--- src/main/java/com/szrthk/facility_booking/domain/Facility.java
+++ src/main/java/com/szrthk/facility_booking/domain/Facility.java
@@ -1,4 +1,46 @@
 package com.szrthk.facility_booking.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+@Document(collection = "facilities")
+@CompoundIndex(name="tenant_name_unique", def="{ 'tenantId': 1, 'name': 1 }", unique = true)
 public class Facility {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String tenantId;
+
+    @NotBlank
+    private String name;
+
+    @Positive
+    private int capacity;
+
+    @Positive
+    private int slotDurationMinutes;
+
+    @NotBlank
+    private String openHours;
+
+    @Builder.Default
+    private Rules rules =  new Rules();
+    @Data @NoArgsConstructor @AllArgsConstructor @Builder
+    public static class Rules {
+        @Positive @Builder.Default
+        private int maxPerDay = 2;
+
+        @Positive @Builder.Default
+        private int coolDownMins = 60;
+    }
 }
--- src/main/java/com/szrthk/facility_booking/domain/Slot.java
+++ src/main/java/com/szrthk/facility_booking/domain/Slot.java
@@ -1,4 +1,32 @@
 package com.szrthk.facility_booking.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "slots")
+@CompoundIndex(name="tenant_facility_start_unique", def="{ 'tenantId':1, 'facilityId':1, 'start':1 }", unique = true)
 public class Slot {
+    @Id
+    private String id;
+
+    private String tenantId;
+    private String facilityId;
+
+    private Instant start;   // stored in UTC
+    private Instant end;
+
+    private int maxCapacity;
+    @Builder.Default
+    private int bookedCount = 0; // will be atomically incremented during booking
 }
